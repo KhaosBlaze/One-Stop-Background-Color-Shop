7e1c05389eafb7be29bdcfe849d5726cfa5c6bf3
Charles Sherburne <sherburne.chuck@gmail.com>
2019-11-14 11:01:35 -0500

    Replaced css echo with require

--- charless-one-stop-bg-color-shop.php
+++ charless-one-stop-bg-color-shop.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package One_Stop_Background_Color_Shop
- * @version 0.0.1
+ * @version 0.0.2
  */
 /*
 Plugin Name: One Stop Background Color Shop
 Plugin URI: TBD
 Description: This plugin allows for a one click modification of your site's background color.
 Author: Charles Sherburne
-Version: 0.0.1
+Version: 0.0.2
 Author URI: TBD
 */
 
@@ -22,23 +22,29 @@ add_action( 'admin_init', 'osbcs_register_settings' );
 
 //Setup options page to be able to change color
 function osbcs_options_page() {
-  add_options_page('One Stop Background Color Shop!', 'One Stop Background Color Shop', 'manage_options', 'one-stop-bg-color-shop', 'osbcs_option_page_html');
+    add_options_page(
+	'One Stop Background Color Shop!', 
+	'One Stop Background Color Shop', 
+	'manage_options', 
+	'one-stop-bg-color-shop', 
+	'osbcs_option_page_html'
+    );
 }
 
 add_action('admin_menu', 'osbcs_options_page');
 
 //Setup css code to change background color to chosen color
 function osbcs_custom_css(){
-  $color = get_option('osbcs_color'); //Fetch color
-  echo "<style>body{background-color: ".$color." !important}</style>";
+  $color = get_option('osbcs_color'); //Fetch colori
+  require 'css/osbcs.php'; //Fetch css
 }
 
 add_action( 'wp_enqueue_scripts', 'osbcs_custom_css', 999); //Load CSS after theme
 
 function osbcs_option_page_html()
 {
-  if ( ! current_user_can( 'manage_options' ) ) {return; }//Kick the user back if they don't have the correct permissions
-?>
+ //Kick the user back if they don't have the correct permissions
+ if ( ! current_user_can( 'manage_options' ) ) {return; }?>
  <!---Create html form to submit color--->
  <div class="wrap">
    <h1><?php echo esc_html( get_admin_page_title() );?></h1>
